eb55a91cb37a3b8daf12c4fcb20cad852fac218f
yjkmust <yjkmust@gmail.com>
2019-04-25 23:41:54 +0800

    Initial commit

---
+++ src/main/java/com/yjkmust/yjmall/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.yjkmust.yjmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class DemoController {
+ 
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
